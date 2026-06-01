982e4fc29385be01283d6b85cf67ad7e6256a578
Daffa Arya Kesuma <21523010@students.uii.ac.id>
2026-06-01 17:37:04 +0700

    feat: restrict allowed origins in CORS configuration and add cache clearing route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/clear-cache', function() {
+    Artisan::call('optimize:clear');
+    return 'Cache aman bre!';
 });
