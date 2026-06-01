6c9669930cc8627c61aff130d85357b6f7af150d
Daffa Arya Kesuma <21523010@students.uii.ac.id>
2026-06-01 17:31:52 +0700

    feat: add trustProxies middleware configuration to enhance security

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         // Avoid Route [login] errors when unauthenticated requests hit auth middleware.
+        $middleware->trustProxies(at: '*');
         $middleware->redirectGuestsTo('/');
 
         $middleware->alias([
